4a3e23ada70647ba8504caa95fb4b9ffa3b2594a
AID-IOT-MAYANK <mayank@aid-iot.com>
2023-12-28 08:16:37 +0530

    First Commit

---
+++ target/debug/deps/invaders_game-6267dc21132e78a0.d
@@ -0,0 +1,5 @@
+/Users/mayankjain/Documents/Web Projects/Rust_Projs/invaders_game/target/debug/deps/libinvaders_game-6267dc21132e78a0.rmeta: src/main.rs
+
+/Users/mayankjain/Documents/Web Projects/Rust_Projs/invaders_game/target/debug/deps/invaders_game-6267dc21132e78a0.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/invaders_game-964db6fe258552da.d
@@ -0,0 +1,5 @@
+/Users/mayankjain/Documents/Web Projects/Rust_Projs/invaders_game/target/debug/deps/libinvaders_game-964db6fe258552da.rmeta: src/main.rs
+
+/Users/mayankjain/Documents/Web Projects/Rust_Projs/invaders_game/target/debug/deps/invaders_game-964db6fe258552da.d: src/main.rs
+
+src/main.rs:
